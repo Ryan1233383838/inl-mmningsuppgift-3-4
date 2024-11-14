5924c6d4bc41c74f4415c15cc142d6c9aaf0de4a
ryhu06001 <ryhu06001@E-PW05CGRC>
2024-11-14 15:39:28 +0100

    Add project files.

---
+++ inlämmningsuppgift 3,4/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace Uppgift_3_4
+    {
+        class Program
+        {
+            static void Main(string[] args)
+            {
+               
+                Console.Write("Ange antalet minuter för låten: ");
+                int minuter = int.Parse(Console.ReadLine());
+
+                Console.Write("Ange antalet sekunder för låten: ");
+                int sekunder = int.Parse(Console.ReadLine());
+
+                
+                int totalTidISekunder = minuter * 60 + sekunder;
+
+               
+                int minstaTid = 2 * 60 + 45;   
+                int maxstaTid = 4 * 60 + 20;    
+                
+                if (totalTidISekunder >= minstaTid && totalTidISekunder <= maxstaTid)
+                {
+                    Console.WriteLine("Låten får spelas på radiostationen.");
+                }
+                else
+                {
+                    Console.WriteLine("Låten får inte spelas på radiostationen.");
+                }
+            }
+        }
+    }
